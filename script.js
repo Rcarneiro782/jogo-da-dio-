8b37cf9eeb91243e0e4914f7e98803c41efd7bce
rcarneiro782 <rcarneiro782@gmail.com>
2025-06-28 09:52:44 -0300

    mudando tudo

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 //git commit -m "mensagem do que fez"
 //git push
 
-class superGuerreiro{
-    constructor(guerreiro, mago, monge, ninja){
-     username: ["Cassio", "Caio", "Rodrigo", "Brenda"];
-     age: [55, 25, 33, 17]
-     typ: ["Guerreiro", "mago", "Monge", ,"Ninja"]
-     atack: ["Espada", "magia"," artes marciais", "shuriken" ]
+class superHeroi{
+    constructor(name, age, typ){
+     this.name = name;
+     this.age= age;
+     this.typ = typ
     }
 }
 
-superGuerreiro(`O ${typ[0]} atacou usando ${atack[0]}`)
