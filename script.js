58d1d95c353c363e46db2e03c6cf1d07c82695d0
rcarneiro782 <rcarneiro782@gmail.com>
2025-06-28 09:43:26 -0300

    ewstou perdido

---
+++ script.js
@@ -0,0 +1,14 @@
+// git add .
+//git commit -m "mensagem do que fez"
+//git push
+
+class superGuerreiro{
+    constructor(guerreiro, mago, monge, ninja){
+     username: ["Cassio", "Caio", "Rodrigo", "Brenda"];
+     age: [55, 25, 33, 17]
+     typ: ["Guerreiro", "mago", "Monge", ,"Ninja"]
+     atack: ["Espada", "magia"," artes marciais", "shuriken" ]
+    }
+}
+
+superGuerreiro(`O ${typ[0]} atacou usando ${atack[0]}`)
